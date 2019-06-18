6fdd3d8f55a95b318b8d2d887bc6ad60327beb22
Z. Tang <chuang.tang@yale.edu>
2019-06-17 21:41:10 -0500

    add analyser node + canvas

--- audio_test/index.js
+++ audio_test/index.js
@@ -11,10 +11,90 @@ const source = offlineCtx.createBufferSource();
 
 const btn = document.querySelector('button');
 
+class Canvas {
+  constructor(elem){
+    this.canvas = document.querySelector(elem);
+    
+    if(this.canvas.getContext){
+      this.ctx = this.canvas.getContext('2d');
+    }
+
+    this.width = 1024;
+    this.height = 300;
+    
+    console.log(this.ctx);
+  }
+
+  clear(){
+    this.ctx.fillStyle = "rgba(0, 120, 0, 0.01)";
+    this.ctx.fillRect(0, 0, this.width, this.height);
+  }
+}
+
+const HEIGHT = 300;
+
+/* Returns a draw() function that you can call to start the
+   drawing process
+*/
+function setUp(analyser, canvas){
+  if (!analyser){
+    console.log("Analyser node not set up yet...");
+    return;
+  }
+
+  analyser.fftSize = 2048;
+  var bufferLength = analyser.frequencyBinCount;
+  var dataArray = new Uint8Array(bufferLength);
+  console.log(bufferLength);
+
+  let f = canvas.ctx;
+  
+  let print = 0;
+  draw = () => {
+    if (!analyser){
+      console.log("ok");
+      return;
+    }
+
+    analyser.getByteFrequencyData(dataArray);
+    requestAnimationFrame(draw);
+
+    canvas.clear();
+    f.lineWidth = 2;
+    f.strokeStyle = 'white';
+    f.beginPath();
+
+    if (print++ % 10 == 0) console.log(dataArray.slice(0, 50));
+
+    let transform = x => x / 128.0;
+    let sliceWidth = 1;
+    let x = 0;
+    for (let i=0; i < bufferLength; i++){
+      var v = transform(dataArray[i])
+      var y = v * HEIGHT / 2;
+
+      if (i==0){
+        f.moveTo(x, y);
+      }else{
+        f.lineTo(x, y);
+      }
+
+      x += sliceWidth;
+    }
+
+    f.lineTo(canvas.width, canvas.height/2);
+    f.stroke();
+  }
+
+  return draw;
+}
+
 
 function getData(){
+  let canvas = new Canvas('#output');
+  
   let request = new XMLHttpRequest();
-  request.open('GET', 'viper.ogg', true);
+  request.open('GET', 'StreetDrone.mp3', true);
   request.responseType = 'arraybuffer';
 
   request.onload = () => {
@@ -46,16 +126,23 @@ function getData(){
             
             // Loop through the 4096 samples
             for (var s = 0; s < inputBuffer.length; s++){
-              outputData[s] = inputData[s] + (Math.random() * 2 - 1) * 0.05
+              outputData[s] = inputData[s];
             }
           }
         }
 
+        // Create an AnalyserNode
+        let analyser = audioCtx.createAnalyser();
+        const drawfn = setUp(analyser, canvas);
+        drawfn();
+
         // Old: 
-        song.connect(audioCtx.destination);
+        // song.connect(audioCtx.destination);
         // New:
-        // song.connect(scriptNode);
-        // scriptNode.connect(audioCtx.destination);
+
+        song.connect(scriptNode);
+        scriptNode.connect(analyser);
+        analyser.connect(audioCtx.destination);
 
         // song.onended = () => {
         //   song.disconnect(scriptNode);
@@ -95,4 +182,4 @@ function grabUserCamera(){
     console.log("Nope!")
   }
 }
-grabUserCamera();
+// grabUserCamera();
